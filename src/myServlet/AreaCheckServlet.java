167fef5bb26d36281290c6d7f5242b8cde6d44ab
Nikolai <smirnovsg15@gmail.com>
2015-04-25 16:10:48 +0300

    start

---
+++ src/myServlet/AreaCheckServlet.java
@@ -0,0 +1,140 @@
+package myServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class AreaCheckServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        String x_in = new String("0");
+        String y_in = new String("0");
+        String r_in = new String("0");
+
+        PrintWriter out = response.getWriter();
+
+        try {
+
+            x_in = request.getParameter("x");
+            y_in = request.getParameter("y");
+            r_in = request.getParameter("r");
+
+        } catch (NullPointerException npe) {
+
+            request.getRequestDispatcher("/index.jsp").forward(request, response);
+
+        }
+
+            if (isNumeric(x_in) && isNumeric(y_in) && isNumeric(r_in)) {
+
+                /* HERE OUT OR REPLACE USE TAMPLATE */
+
+            } else {
+
+                request.getRequestDispatcher("/index.jsp").forward(request, response);
+
+            }
+
+        out.close();
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        /* HERE WE NEED FORWARD TO OUR JSP */
+
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        request.getRequestDispatcher("/index.jsp").forward(request, response);
+
+    }
+
+    /* Check for number */
+
+    public static boolean isNumeric(String str) {
+
+        try {
+
+            double d = Double.parseDouble(str);
+
+        } catch(NumberFormatException nfe) {
+
+            return false;
+
+        }
+
+        return true;
+
+    }
+
+    /* Function to get number of quarter */
+
+    private int getNumberQuarter(float x, float y){
+
+        if(x > 0)
+            return (y >= 0) ? 1 : 4;
+
+        if(x == 0)
+            return (y == 0) ? 0 : ( (y > 0) ? 1 : 4 );
+
+        else
+            return (y > 0) ? 2 : 3;
+
+    }
+
+/* Function to check in Sub Area */
+
+    private boolean isInSubArea(float x, float y, float r, int number){
+
+        switch(number){
+
+            case 0:
+                return true;
+
+            case 1:
+
+                return ( Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)) <= r ) ? true : false;
+
+            case 2:
+
+                return ( ( (x + r) / 2. ) >= y ) ? true : false;
+
+            case 3:
+
+                if( x >= -r ) {
+
+                    if ( y >= -r ) {
+
+                        return true;
+
+                    }
+                }
+
+                return false;
+
+        }
+
+        return false;
+
+    }
+
+/* Function to check point */
+
+    public boolean isInArea(float x, float y, float r){
+
+        int numberOfQuarter = getNumberQuarter(x, y);
+
+        return isInSubArea(x, y, r, numberOfQuarter);
+
+    }
+
+}
---
+++ src/myServlet/ControllerServlet.java
@@ -0,0 +1,40 @@
+package myServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ControllerServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        /* HERE WE NEED FORWARD TO OUR JSP */
+
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        request.getRequestDispatcher("/index.jsp").forward(request, response);
+
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        if( (request.getParameter("x") == null) || (request.getParameter("y") == null) || (request.getParameter("r") == null) ) {
+
+            request.getRequestDispatcher("/index.jsp").forward(request, response);
+
+        } else {
+
+            request.getRequestDispatcher("/result").forward(request, response);
+
+        }
+
+    }
+}
